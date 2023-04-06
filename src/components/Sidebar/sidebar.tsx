1f90042fc0378cf09ac274615b26be8949ab8b7a
kato-rakuweb <kato@rakuweb.jp>
2023-04-06 13:16:09 +0900

    ロジック

--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 import styled from '@emotion/styled';
-import { Link } from '@chakra-ui/react';
+import { Box, Link } from '@chakra-ui/react';
 
 const Sidebar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,19 +13,29 @@ const Sidebar: React.FC = () => {
   return (
     <SidebarContainer isOpen={isOpen}>
       <SidebarContent isOpen={isOpen}>
-        <Link href={'/'} color={'white'}>
-          <span className="kigou">🏠</span>
-          <span className="moji">ホーム</span>
-        </Link>
-        <Link href={'/'} color={'white'}>
-          <span className="kigou">🆗</span>
-          <span className="moji">サイト管理</span>
-        </Link>
+        <Box className="box1">
+          <Link className="link" href={'/'} color={'white'}>
+            🏠
+          </Link>
+          <Link href={'/'} color={'white'}>
+            🆗
+          </Link>
+          <SidebarToggle onClick={toggleSidebar}>
+            {isOpen ? '>' : '<'}
+          </SidebarToggle>
+        </Box>
+        <Box className="box2">
+          <Link href={'/'} color={'white'} whiteSpace={'nowrap'}>
+            🏠 ホーム
+          </Link>
+          <Link href={'/'} color={'white'} whiteSpace={'nowrap'}>
+            🆗 サイト管理
+          </Link>
+          <SidebarToggle onClick={toggleSidebar}>
+            {isOpen ? '>' : '<'}
+          </SidebarToggle>
+        </Box>
       </SidebarContent>
-
-      <SidebarToggle onClick={toggleSidebar}>
-        {isOpen ? '>' : '<'}
-      </SidebarToggle>
     </SidebarContainer>
   );
 };
@@ -33,9 +43,8 @@ const Sidebar: React.FC = () => {
 export default Sidebar;
 
 const SidebarContainer = styled.div<{ isOpen: boolean }>`
-  width: ${(props) => (props.isOpen ? '200px' : '60px')};
+  width: ${(props) => (props.isOpen ? '200px' : '80px')};
   height: 100%;
-  background-color: #333;
   position: fixed;
   left: 0;
   top: 0;
@@ -46,18 +55,59 @@ const SidebarToggle = styled.div`
   color: #fff;
   cursor: pointer;
   padding: 10px;
-  text-align: center;
+  /* text-align: center; */
   user-select: none;
 `;
 
+const fadeIn = `
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+      visibility: visible;
+    }
+  }
+`;
+
+const fadeOut = `
+  @keyframes fadeOut {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity:0;
+      visibility: hidden;
+    }
+  }
+`;
+
 const SidebarContent = styled.div<{ isOpen: boolean }>`
+  ${fadeIn}
+  ${fadeOut}
   display: flex;
   flex-direction: column;
   gap: 10px;
   padding: 10px 0;
-  opacity: ${(props) => (props.isOpen ? 1 : 0)};
-  transition: opacity 0.3s;
-
+  position: relative;
+  .box1 {
+ 
+    animation: ${(props) => (props.isOpen ? 'fadeOut 0.3s' : 'fadeIn 0.3s')};
+    animation-fill-mode: forwards;
+    background-color: #333;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+  }
+  .box2 {
+    animation: ${(props) => (props.isOpen ? 'fadeIn 0.3s' : 'fadeOut 0.3s')};
+    animation-fill-mode: forwards;
+    background-color: #333;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+  }
   a {
     display: flex;
     align-items: center;
@@ -65,26 +115,11 @@ const SidebarContent = styled.div<{ isOpen: boolean }>`
     padding: 10px;
     cursor: pointer;
     text-decoration: none;
-
     &:hover {
       background-color: rgba(255, 255, 255, 0.1);
     }
 
-    span.kigou {
-      color: #fff;
-      display: inline;
-    }
 
-    span.moji {
-      color: #fff;
-      display: ${(props) => (props.isOpen ? 'inline' : 'none')};
-      transition: display 0.3s;
     }
-
-    /* ${SidebarContainer}:hover & {
-      span.moji {
-        display: inline;
-      }
-    } */
   }
 `;
