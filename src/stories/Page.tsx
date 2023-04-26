da68cbe7ebb1ecda05b30ad00f2af50b3c7feaad
rakuweb <dev@rakuweb.jp>
2023-04-26 09:59:38 +0900

    refactor:shower: storybook内の不要コードを削除

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import { Header } from './Header';
-import { Box } from '@chakra-ui/react';
 import './page.css';
 
 type User = {
   name: string;
 };
 
-export const Page: React.VFC = () => {
+export const Page: React.FC = () => {
   const [user, setUser] = React.useState<User>();
 
   return (
