675bbc285433f80ee44e01c972cf944302f311da
Shoma <takano@rakuweb.jp>
2023-04-28 18:14:38 +0900

    サイドバーとページネーションを修正、及び追加

--- src/components/Pagination/Pagecircle/presenter.tsx
+++ src/components/Pagination/Pagecircle/presenter.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react';
-import { Flex } from '@chakra-ui/react';
+import { Flex, FlexProps } from '@chakra-ui/react';
 
-export type PresenterProps = { pagenumber: string };
+export type PresenterProps = { pagenumber: number } & FlexProps;
 
-export const Presenter: FC<PresenterProps> = ({ pagenumber }) => {
+export const Presenter: FC<PresenterProps> = ({ pagenumber, ...props }) => {
   return (
     <Flex
       fontSize={`${14 / 19.2}vw`}
@@ -17,10 +17,11 @@ export const Presenter: FC<PresenterProps> = ({ pagenumber }) => {
       justify={`center`}
       mr={`${10 / 19.2}vw`}
       _hover={{
-        transition: `0.3s`,
+        transition: `0.1s`,
         cursor: `pointer`,
         border: `1px solid #49BAC0`,
       }}
+      {...props}
     >
       {pagenumber}
     </Flex>
