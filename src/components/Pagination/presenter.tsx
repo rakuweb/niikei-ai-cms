bedd45a194658ed9b39f33e7536b959eda61dd6b
ShomaTakano <110518329+ShomaTakano@users.noreply.github.com>
2023-05-02 15:10:52 +0900

    サイドバーとページネーションの修正 (#7)
    * 登録情報一覧ページ作成途中
    ログインのLinkをchakrauiのものに変更
    
    * 登録情報一覧(crawlers)ページview作成
    
    * 使っていないimport削除
    
    * 修正
    InternalLinkの変更、listの型定義など行いました
    
    * アカウント情報ページviewを作成しました
    
    * 未使用import削除
    
    * 入力欄の枠線色変更
    
    * サイドバーとページネーションを修正、及び追加

--- src/components/Pagination/presenter.tsx
+++ src/components/Pagination/presenter.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Box, Flex } from '@chakra-ui/react';
 import { Pagecircle } from './Pagecircle';
 
 export type PresenterProps = Record<string, unknown>;
 
 export const Presenter: FC = () => {
+  const [pagenumber, setPagenumber] = useState(1);
   return (
     <Flex
       w={`fit-content`}
@@ -16,17 +17,45 @@ export const Presenter: FC = () => {
       alignItems={`center`}
       fontSize={`${16 / 19.2}vw`}
     >
-      <Pagecircle pagenumber="1" />
-      <Pagecircle pagenumber="2" />
-      <Pagecircle pagenumber="3" />
-      <Pagecircle pagenumber="4" />
-      <Box ml={`${10 / 19.2}vw`} mr={`${17 / 19.2}vw`}>{`…`}</Box>
       <Box
+        display={pagenumber == 1 ? `none` : `block`}
+        onClick={() => setPagenumber(pagenumber - 1)}
         _hover={{
           cursor: `pointer`,
           color: `#49BAC0`,
           borderBottom: `1px`,
-          transition: `0.3s`,
+        }}
+      >{`戻る`}</Box>
+      <Box
+        display={pagenumber == 1 ? `none` : `block`}
+        ml={`${10 / 19.2}vw`}
+        mr={`${17 / 19.2}vw`}
+      >{`…`}</Box>
+
+      <Pagecircle
+        pagenumber={pagenumber}
+        _hover={{}}
+        border={`1px solid #49BAC0`}
+      />
+      <Pagecircle
+        pagenumber={pagenumber + 1}
+        onClick={() => setPagenumber(pagenumber + 1)}
+      />
+      <Pagecircle
+        pagenumber={pagenumber + 2}
+        onClick={() => setPagenumber(pagenumber + 2)}
+      />
+      <Pagecircle
+        pagenumber={pagenumber + 3}
+        onClick={() => setPagenumber(pagenumber + 3)}
+      />
+      <Box ml={`${10 / 19.2}vw`} mr={`${17 / 19.2}vw`}>{`…`}</Box>
+      <Box
+        onClick={() => setPagenumber(pagenumber + 1)}
+        _hover={{
+          cursor: `pointer`,
+          color: `#49BAC0`,
+          borderBottom: `1px`,
         }}
       >{`次へ`}</Box>
     </Flex>
