75c7239007c6026bde91a7c0834f9a5e2771d293
kato-rakuweb <kato@rakuweb.jp>
2023-05-15 13:15:37 +0900

    修正

--- src/components/New/presenter.tsx
+++ src/components/New/presenter.tsx
@@ -9,7 +9,7 @@ import { NewForm } from 'components/organisms/NewForm';
 
 export type PresenterProps = Record<string, unknown>;
 
-export const Presenter: FC = () => {
+export const Presenter: FC<PresenterProps> = () => {
   const title = `登録情報一覧`;
 
   return (
