df85cbb376a278fd69a0690c5d9a275621a93f39
rakuweb <dev@rakuweb.jp>
2023-06-14 16:52:37 +0900

    feat:sparkles: 認証機能修正対応

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -2,15 +2,7 @@ import { Box } from '@chakra-ui/react';
 import { Login } from 'components/Login';
 import { NextPage } from 'next';
 
-type SigninProps = {
-  data: {
-    email: string;
-    password: string;
-    name: string;
-  };
-};
-
-const Signin: NextPage<SigninProps> = () => {
+const Signin: NextPage = () => {
   return (
     <Box w="100vw" h="100vh" bg="#EAEAEA">
       <Login />
