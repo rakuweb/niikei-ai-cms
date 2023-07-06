fded03fddc5228a1761228d89657c8bd2f217a30
rakuweb <dev@rakuweb.jp>
2023-07-06 19:59:06 +0900

    fix:bug: 削除ボタンの処理の追加

--- src/components/organisms/FortunesTable/presenter.tsx
+++ src/components/organisms/FortunesTable/presenter.tsx
@@ -13,7 +13,6 @@ import {
 import { Text } from 'components/texts/Text';
 import { WideButton } from 'components/Button/WideButton';
 import { GrayButton } from 'components/Button/GrayButton';
-import { InternalLink } from 'components/links/InternalLink';
 import { css } from '@emotion/react';
 import { Timestamp } from 'firebase/firestore';
 import dayjs from 'dayjs';
@@ -72,11 +71,11 @@ export const Presenter: FC<PresenterProps> = () => {
   }, []);
 
   const handleDelete = async (id: string) => {
-    console.log('Handle delete for id:', id);
     await deleteFortunesLogs(companyID, id).catch((err) => {
       console.error(err);
       alert('削除に失敗しました。時間経ってからもう一度お試しください。');
     });
+    alert('画像を削除しました。');
   };
   const handleSwitchChange = async (id: string, used: boolean) => {
     const checkOnly = (id: string) => {
@@ -195,7 +194,7 @@ export const Presenter: FC<PresenterProps> = () => {
                           <Box display={'flex'} justifyContent={'space-around'}>
                             <ExternalLink href={log.url}>
                               <WideButton
-                                text={`編集する`}
+                                text={`確認する`}
                                 w={`${140 / 19.2}vw`}
                               />
                             </ExternalLink>
