1d556e6d6b18f43a81f332084b592781df02f726
Kumazawa <dev@rakuweb.jp>
2023-07-20 17:43:53 +0900

    feat:sparkles: 自動要約を追加 (#72)

--- src/pages/api/create-document.ts
+++ src/pages/api/create-document.ts
@@ -1,11 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { docs_v1, drive_v3, google } from 'googleapis';
+import { ChatCompletionRequestMessageRoleEnum } from 'openai';
 
 import {
   GOOGLE_APPLICATION_CREDENTIALS_CREATE_DOCUMENT,
   GOOGLE_TEMPLATE_DOCUMENT_ID,
   GOOGLE_PARENT_FOLDER,
 } from 'constants/env';
+import { openAiRequest } from 'lib/openai';
 
 const credentials = JSON.parse(
   Buffer.from(
@@ -50,21 +52,45 @@ const copyDocument = async (req: NextApiRequest, res: NextApiResponse) => {
 
     const url = document.data.webViewLink;
 
+    // chatGPT
+    let inputText = '';
+    if (text) {
+      const message = `下記の文章を記事風に要約してください。\n\n${text}`;
+      const requestMessages = [
+        { content: message, role: ChatCompletionRequestMessageRoleEnum.User },
+      ];
+      const resChat = await openAiRequest(requestMessages);
+
+      if (!resChat) {
+        inputText = `ChatGPT APIへの接続に失敗したため、要約文を生成できませんでした。
+        代わりに原文を挿入しました。
+
+${text}
+
+----------------------------`;
+      } else {
+        const choise = resChat.choices[0];
+        inputText = `${choise.message?.content ?? ''}
+
+----------------------------`;
+      }
+    }
+
     const docs: docs_v1.Docs = google.docs({
       version: 'v1',
       auth,
     });
-    const requests = text
+    const requests = inputText
       ? [
-          {
-            insertText: {
-              location: {
-                index: 1,
-              },
-              text,
+        {
+          insertText: {
+            location: {
+              index: 1,
             },
+            text: inputText,
           },
-        ]
+        },
+      ]
       : [];
 
     if (requests.length > 0) {
