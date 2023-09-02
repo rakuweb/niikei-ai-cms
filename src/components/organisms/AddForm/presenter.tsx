99050de4e4ab2a2fc48ec4b016351c5434f3b715
rakuweb <dev@rakuweb.jp>
2023-09-02 15:02:31 +0900

    fix:+1:本番環境用接続設定

--- src/components/organisms/AddForm/presenter.tsx
+++ src/components/organisms/AddForm/presenter.tsx
@@ -96,9 +96,9 @@ export const Presenter: FC<PresenterProps> = ({ data, id }) => {
       setCategories((prev) =>
         resCategories
           ? resCategories.map((category) => ({
-              id: category.id,
-              name: category.name,
-            }))
+            id: category.id,
+            name: category.name,
+          }))
           : prev
       );
     };
@@ -107,6 +107,7 @@ export const Presenter: FC<PresenterProps> = ({ data, id }) => {
   }, []);
 
   useEffect(() => {
+    if (!data) return;
     reset({
       name: data.name,
       url: data.url,
